c5456196c6add2aa3a4dc60693ddb3fab20453cd
lfreitas <lfreitas@college.harvard.edu>
2013-05-07 17:39:21 -0400

    works on alice in wonderland, working on performance issues

--- code/data_count.py
+++ code/data_count.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+from unidecode import unidecode
 
 def initials(input, initialDict):
     for line in input:
@@ -9,15 +10,19 @@ def initials(input, initialDict):
     return initialDict
 
 def bigrams(input,ngrams):
+    ngrams = {}
     for line in input:
         line= line.decode('utf-8')
         line = line.strip("\n")
         for k in range(0, len(line) - 1):
             char1 = line[k].encode('utf-8', 'ignore')
             char2 = line[k+1].encode('utf-8', 'ignore')
-            if char1 not in ngrams.keys():
-                ngrams[char1] = defaultdict(int)
-            ngrams[char1][char2] += 1
+            if char1 not in ngrams:
+                ngrams[char1] = {}
+            if char2 not in ngrams[char1]:
+                ngrams[char1][char2] = 1
+            else:
+                ngrams[char1][char2] += 1
             
     return ngrams
 
@@ -29,16 +34,40 @@ def createS(input,S):
                 S.append(char)
     return S
 
+def createO(input,O):
+    for line in input:
+        line = line.decode('utf-8')
+        for char in line:
+            pin = unidecode(char).lower().strip(' ')
+            if pin not in O and pin.isalpha():
+                O.append(pin)
+    return O
+
+def createB(input, O):
+    B = {}
+    for line in input:
+        line = line.decode('utf-8')
+        for char in line:
+            if char not in B:
+                B[char] = unidecode(char).lower().strip(' ')
+    return B
+
 def main():
     input = sys.argv[1]
     f = open(input, 'r').readlines()
+    out = open("output.txt", "w")
     S = createS(f,[])
-    #print S
+    O = createO(f,[])
+    #out.write(str(S))
     initialMatrix = defaultdict(int)
     pi = initials(f, initialMatrix)
     #print pi
     A = bigrams(f, {})
-    print A
+    #out.write(str(A))
+    B = createB(f, O)
+    out.write(str(A))
+    #print A
+
     
 if __name__ == '__main__':
     main()
--- viterbi/viterby.py
+++ viterbi/viterby.py
@@ -1,7 +1,78 @@
 # coding: UTF-8
+import sys
 from itertools import *
-def argmax(pairs):
-    return max(enumerate(x), key=lambda x:x[1])[0]
+from unidecode import unidecode
+from cjklib.characterlookup import CharacterLookup
+
+def tokenize(input, output):
+	try:
+		text = open(input, 'r').readlines()
+	except IOError:
+		print "IOError: could not open", input
+		sys.exit()
+
+	cjk = CharacterLookup('T')
+	out = open(output, 'w')
+
+	for line in text:
+		line = line.decode('utf-8')
+		new_line = ""
+		for char in line:
+			pinyin = cjk.getReadingForCharacter(char, 'Pinyin')
+			if pinyin:
+				new_line += char
+		new_line += '\n'
+		out.write(new_line.encode('utf-8'))
+	out.close()
+
+def initials(input, dict):
+    for line in input:
+        line = line.decode('utf-8')
+        if line[0] not in dict:
+        	dict[line[0]] = 1
+        else:
+        	dict[line[0]] += 1
+    return dict
+
+def bigrams(input, ngrams):
+    for line in input:
+        line = line.decode('utf-8').strip("\n")
+        for k in range(0, len(line) - 1):
+            char1 = line[k].encode('utf-8', 'ignore')
+            char2 = line[k + 1].encode('utf-8', 'ignore')
+            if char1 not in ngrams:
+                ngrams[char1] = {}
+            if char2 not in ngrams[char1]:
+                ngrams[char1][char2] = 1
+            else:
+                ngrams[char1][char2] += 1 
+    return ngrams
+
+def createS(input,S):
+    for line in input:
+        line = line.decode('utf-8')
+        for char in line:
+        	if char not in S:
+        		S.append(char)
+    return S
+
+def createO(input,O):
+    for line in input:
+        line = line.decode('utf-8')
+        for char in line:
+            pin = unidecode(char).lower()
+            if pin not in O:
+                O.append(pin)
+    return O
+
+def createB(input, B):
+    B = {}
+    for line in input:
+        line = line.decode('utf-8')
+        for char in line:
+            if char not in B:
+                B[char] = unidecode(char).lower().strip(' ')
+    return B
 
 def viterbi(O, S, pi, Y, A, B):
 	"""
@@ -32,15 +103,30 @@ def viterbi(O, S, pi, Y, A, B):
 
 	#print "delta 1:"
 	for state in S:
-		T[1][state, 1] = pi[state] * B[state][Y[0]]
+		if B[state] == Y[0]:
+			Beta = 1
+		else:
+			Beta = 0
+		T[1][state, 1] = (pi[state] + 1) * Beta if state in pi else Beta
 		T[2][state, 1] = 0
 		#print "\ndelta1(" + str(state.encode('utf-8', 'ignore')) + ") = " + str(T[1][state, 1])
-	#print "chosen: " + str(S[T[2][state, 1]].encode('utf-8', 'ignore'))
+		#print "chosen: " + str(S[T[2][state, 1]].encode('utf-8', 'ignore'))
 
 	for i in range(2, t + 1):
 		#print "\ndelta " + str(i)
 		for state in S:
-			values = [T[1][x, i - 1] * A[x][state] * B[state][Y[i - 1]] for x in S]
+			if B[state] == Y[i - 1]:
+				Beta = 1
+			else:
+				Beta = 0
+			values = []
+			for x in S:
+				if x in A:
+					if state in A[x]:
+						Alpha = A[x][state]
+				else:
+					Alpha = 0.5
+				values.append(T[1][x, i - 1] * Alpha * Beta)
 			T[2][state, i], T[1][state, i] = max(enumerate(values), key=lambda values:values[1])
 			#print "\ndelta" + str(i) + "(" + str(state.encode('utf-8', 'ignore')) + ") = " + str(T[1][state, i])
 			#print "chosen: " + str(S[T[2][state, i]].encode('utf-8', 'ignore'))
@@ -54,27 +140,32 @@ def viterbi(O, S, pi, Y, A, B):
 	for i in range(t, 1, -1):
 		Z[i - 1] = T[2][S[Z[i]], i]
 		X[i - 1] = S[Z[i - 1]].encode('utf-8', 'ignore')
-	
+
 	Y = ""
 	for x in X:
 		Y += X[x]
 
 	return Y
 
-O = ["hen", "wo", "ni"]
-S = [x for x in u'恨你我很']
-pi = {u"很": 0.1, u"我": 0.4, u"你": 0.4, u"恨": 0.1}
-Y = ["wo", "hen", "hen", "ni"]
-A = {}
-A[u"我"] = {u"我": 0.0, u"很": 0.6, u"你": 0.1, u"恨": 0.3}
-A[u"很"] = {u"我": 0.2, u"很": 0.0, u"你": 0.2, u"恨": 0.8}
-A[u"你"] = {u"我": 0.2, u"很": 0.4, u"你": 0.0, u"恨": 0.4}
-A[u"恨"] = {u"我": 0.4, u"很": 0.1, u"你": 0.4, u"恨": 0.1}
+def main():
+    input = sys.argv[1]
+    try:
+    	text = open(input, 'r').readlines()
+    except IOError:
+		print "IOError: could not open", input
+		sys.exit()
 
-B = {}
-B[u"我"] = {"wo": 1, "hen": 0, "ni": 0}
-B[u"很"] = {"wo": 0, "hen": 1, "ni": 0}
-B[u"你"] = {"wo": 0, "hen": 0, "ni": 1}
-B[u"恨"] = {"wo": 0, "hen": 1, "ni": 0}
+    #tokenize("alice.txt", "alice_tokenized.txt")
+    
+    S = createS(text,[])
+    O = createO(text,[])
+    pi = initials(text, {})
+    A = bigrams(text, {})
+    B = createB(text, {})
+    Y = ["wo", "shi", "zhong", "guo", "ren"]
 
-print viterbi(O,S,pi,Y,A,B)	
+    print viterbi(O, S, pi, Y, A, B)
+
+    
+if __name__ == '__main__':
+    main()
