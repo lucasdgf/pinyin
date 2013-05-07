df947dfd4ef5ec20dbfd4e86b4bf0a0f217dd64b
lfreitas <lfreitas@college.harvard.edu>
2013-05-06 22:16:57 -0400

    implemented HHM, works on short examples

--- code/ .py
+++ code/ .py
@@ -1,7 +1,7 @@
 # coding: UTF-8
 import sys
 from unidecode import unidecode
-from cjklib.characterlookup import CharacterLookup
+from cjklib import characterlookup 
 
 def to_pinyin(filename):
 	try:
@@ -10,17 +10,14 @@ def to_pinyin(filename):
 		print "IOError: could not open", filename
 		sys.exit()
 
-	cjk = CharacterLookup('T')
-
-	input = [u'我喜歡他']
+	cjk = characterlookup.CharacterLookup() 
 
 	for line in input:
-		#line = line.decode('utf-8')
+		line = line.decode('utf-8')
 		new_line = ""
 		for char in line:
 			pinyin = cjk.getReadingForCharacter(char, 'Pinyin')
 			if pinyin:
-				print [unidecode(x) for x in pinyin]
 				simplified = unidecode(pinyin[0])
 				new_line += simplified + char + " "
 		line = new_line
